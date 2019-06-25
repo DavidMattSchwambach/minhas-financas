8da1b6ac80c0e8bada09f2c8fa5509073114a186
DavidMattSchwambach <davidschwambach@hotmail.com>
2019-06-25 14:17:55 -0300

    Fazendo o index do conta receber

--- Repository/Estrutura.sql
+++ Repository/Estrutura.sql
@@ -8,8 +8,6 @@ CREATE TABLE contas_pagar(
 	status VARCHAR(100)
 );
 
-SELECT * FROM contas_pagar;
-
 CREATE TABLE contas_receber(
 	id INT PRIMARY KEY IDENTITY(1,1),
 	nome VARCHAR(100)NOT NULL,
@@ -17,4 +15,31 @@ CREATE TABLE contas_receber(
 	tipo VARCHAR(100),
 	descricao VARCHAR(100),
 	status VARCHAR(100)
+);
+
+CREATE TABLE clientes_pessoas_fisicas(
+	id INT PRIMARY KEY IDENTITY(1,1),
+	nome VARCHAR(100)NOT NULL,
+	cpf VARCHAR(100),
+	data_nascimento DATETIME2,
+	rg VARCHAR(100)NOT NULL,
+	sexo BIT
+);
+
+CREATE TABLE clientes_pessoas_juridicas(
+	id INT PRIMARY KEY IDENTITY(1,1),
+	cnpj VARCHAR(100)NOT NULL,
+	razao_social VARCHAR(100)NOT NULL,
+	inscricao_estadual VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE enderecos(
+	id INT PRIMARY KEY IDENTITY(1,1),
+	unidade_federativa VARCHAR(100)NOT NULL,
+	cidade VARCHAR(100)NOT NULL,
+	logradouro VARCHAR(100)NOT NULL,
+	cep DECIMAL(8,2)NOT NULL,
+	numero INT,
+	comnplemento VARCHAR(100)NOT NULL
+
 );
